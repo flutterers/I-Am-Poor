21b221c0455b50b84ca6e351fc4e484968c7a1df
MennaCC <menna.elsisy@gmail.com>
2023-02-16 23:37:54 +0200

    fixing issue #1: support hotreload for emulator + trying custom fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.blueGrey[900],
         appBar: AppBar(
           title: const Center(
             child: Text(
-              "I Am Poor",
-              // style: TextStyle(color: Colors.),
+              "I Am POOR",
+              // style: TextStyle(fontFamily: "Pacifico", fontWeight: FontWeight.w800),
             ),
           ),
           backgroundColor: Colors.indigo,
@@ -21,6 +27,6 @@ void main() {
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
